c3c55512f75e6f6f35b9bca525e912b6f5b0c65a
Ellis <egaraudy@gmail.com>
2019-04-07 12:22:05 -0600

    >update Card component >integrate tachyon styles >create CardList component

---
+++ src/CardList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Card from './Card'
+import {robots} from './robots'
+
+const CardList = () => {
+    return (
+        <div>
+            {robots.map(robot => {
+                return <Card key={robot.email} name={robot.name} email={robot.email} imgUrl={robot.imgUrl} />
+            })}
+        </div>
+    )
+}
+
+export default CardList
---
+++ src/robots.js
@@ -0,0 +1,7 @@
+export const robots = [
+    {name: 'Jon Doe', email: 'doe@gmail.com', imgUrl: 'https://robohash.org/jondoe?200x200'},
+    {name: 'Mike Wallace', email: 'wallace@example.com', imgUrl: 'https://robohash.org/mike?200x200'},
+    {name: 'Avery Mount', email: 'amount@alpine.com', imgUrl: 'https://robohash.org/amount?200x200'},
+    {name: 'Sara Breck', email: 'sara.breck@riversource.com', imgUrl: 'https://robohash.org/sara?200x200'},
+    {name: 'Glenn Alfredson', email: 'glenn@example.io', imgUrl: 'https://robohash.org/glenn?200x200'},
+]
