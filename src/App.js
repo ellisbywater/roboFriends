09531c15ca80bf5d80878323eaf26919b37b2e0a
Ellis <egaraudy@gmail.com>
2019-04-07 13:32:19 -0600

    create SearchBox and search functionality

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import CardList from './CardList';
+import SearchBox from './SearchBox'
+import {robots} from './robots'
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state ={
+            robots,
+            searchField: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchField: event.target.value })
+    }
+
+    render() {
+        const { robots, searchField } = this.state
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchField.toLowerCase())
+        })
+        return (
+            <div className='tc'>
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList robots={filteredRobots}/>
+            </div>
+            
+        )
+    }
+}
+
+export default App
