335758082d5ec099c06e80bde6ab7b917b045ba6
Ellis <egaraudy@gmail.com>
2019-04-13 18:01:12 -0500

    update manifest

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,6 +4,7 @@ const SearchBox = ({searchChange, searchField}) => {
     return (
         <div className='pa2 ma2'>
              <input
+                 aria-label='Search Robots'
                 className='pa3 br2 tc ba b--green bg-lightest-blue'
                 type="search"
                 placeholder="Search Robots"
