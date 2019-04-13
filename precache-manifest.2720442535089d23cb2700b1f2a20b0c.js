5a47b94c74a338834ca2ee680a0bf51afc9d9585
Ellis <egaraudy@gmail.com>
2019-04-13 17:34:40 -0500

    Updates

---
+++ precache-manifest.2720442535089d23cb2700b1f2a20b0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "515ab04aed072d39fb7b",
+    "url": "/roboFriends/static/css/main.24fb7c35.chunk.css"
+  },
+  {
+    "revision": "515ab04aed072d39fb7b",
+    "url": "/roboFriends/static/js/main.72312ee0.chunk.js"
+  },
+  {
+    "revision": "2a748b8689f731107cfe",
+    "url": "/roboFriends/static/js/runtime~main.2740fd8c.js"
+  },
+  {
+    "revision": "419cb8c80e2ca1885bca",
+    "url": "/roboFriends/static/css/2.d72005e4.chunk.css"
+  },
+  {
+    "revision": "419cb8c80e2ca1885bca",
+    "url": "/roboFriends/static/js/2.95393e60.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/roboFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "be4d2cdf918351632d826836f2e3f2f5",
+    "url": "/roboFriends/index.html"
+  }
+];
